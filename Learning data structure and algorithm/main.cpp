290e91b904850aaa1dd2757647f18caf5fd06ec9
jean7155 <jean7155@sina.com>
2019-11-24 17:12:41 +0800

    创建基础 mac项目
    创建基础 一个简单的控制台的 mac项目

---
+++ Learning data structure and algorithm/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Learning data structure and algorithm
+//
+//  Created by mac on 2019/11/22.
+//  Copyright © 2019年 mac20191122. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    int a=0;
+    std::cin >> a ;
+    
+    std::cout << "Hello, World! "<<a <<std::endl;
+    
+    return 0;
+}
