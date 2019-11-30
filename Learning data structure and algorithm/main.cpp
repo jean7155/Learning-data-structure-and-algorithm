e7b272acac46d24ed7b376e02682dcf7283cfa9a
jean7155 <jean7155@sina.com>
2019-12-01 07:04:12 +0800

    动态数组的格式存储稀疏数组

--- Learning data structure and algorithm/main.cpp
+++ Learning data structure and algorithm/main.cpp
@@ -8,17 +8,7 @@
 
 #include <iostream>
 #include <vector>
-struct spareData
-{
-public:
-    int mRow = -1;
-    int mCol = -1;
-    int mValue = -1;
-    void print()
-    {
-        std::cout << mRow << "\t"<<mCol<<"\t"<<mValue<<std::endl;
-    }
-};
+
 int main(int argc, const char * argv[]) {
     //创建一个二维数组，初始化二维数组。
     int chessArr1[11][11];
@@ -51,13 +41,14 @@ int main(int argc, const char * argv[]) {
     std::cout << "sum = "<<sum <<std::endl;
 
     //2.创建对应的稀疏数组
-    std::vector<spareData> sparseArr;
-    //3.给稀疏数组赋值
-    spareData data1;
-    data1.mRow = 11;
-    data1.mCol = 11;
-    data1.mValue = sum;
-    sparseArr.push_back(data1);
+    int** array0 = new int*[3];
+    for (int i=0; i<sum+1; i++) {
+        array0[i] = new int[3]();
+    }
+     //3.给稀疏数组赋值
+    array0[0][0] = 11;
+    array0[0][1] = 11;
+    array0[0][2] = sum;
     
     //3.遍历二维数组将非零值存放到稀疏数组
     int count = 0;  //  记录是第几个非零数据
@@ -65,48 +56,43 @@ int main(int argc, const char * argv[]) {
         for(int j = 0; j < 11; j++) {
             if(chessArr1[i][j]!=0) {
                 count++;
-                
-                spareData data3;
-                data3.mRow = i;
-                data3.mCol = j;
-                data3.mValue = chessArr1[i][j];
-                sparseArr.push_back(data3);
+                array0[count][0] = i;
+                array0[count][1] = j;
+                array0[count][2] = chessArr1[i][j];
             }
         }
     }
     
     //输出稀疏数组的形式
     std::cout<<"得到的稀疏数组为:" << std::endl;
-    for(int i = 0; i < sparseArr.size(); i++) {
-        sparseArr[i].print();
-    }
+    for(int i = 0; i < sum+1; i++) {
+        std::cout << array0[i][0] << "\t"<<array0[i][1]<<"\t"<<array0[i][2]<<std::endl;
+     }
     
     
     //将稀疏数组--->>恢复成原始的二维数组
     //1.先读取稀疏数组的第一行，根据第一行的数据，创建原始的二维数组
-    spareData data0= sparseArr.front();
-    int** des = new int*[data0.mRow];
-    for (int i=0; i<data0.mRow; i++) {
-        des[i] = new int[data0.mCol];
-        for (int j=0; j<data0.mCol; j++) {
-            des[i][j] = 0;
-        }
+    int row = array0[0][0];
+    int col = array0[0][1];
+    int** des = new int*[row];
+    for (int i=0; i<row; i++) {
+        des[i] = new int[col]();
     }
     
     //2.读取稀疏数组的数据赋给二维数组
-    for(int i = 1; i <data0.mValue+1; i++) {
-        spareData data = sparseArr[i];
-        int x = data.mRow;
-        int y = data.mCol;
-        int value = data.mValue;
+    int sun1 = array0[0][2];
+    for(int i = 1; i <sun1+1; i++) {
+        int x = array0[i][0];
+        int y = array0[i][1];
+        int value = array0[i][2];
         des[x][y] = value;
     }
     
     //输出恢复后的二维数组
     std::cout<< "恢复后的原始数组" << std::endl;
     
-    for(int i = 0; i < data0.mRow; i++) {
-        for(int j = 0; j < data0.mCol; j++) {
+    for(int i = 0; i < array0[0][0]; i++) {
+        for(int j = 0; j < array0[0][1]; j++) {
             std::cout << des[i][j] << "\t";
         }
         std::cout << std::endl;
